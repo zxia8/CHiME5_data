d63bbfb2fe2246c31e624d5645d138b00797f1ba
zxia <zxia8@sheffield.ac.uk>
2019-07-30 22:59:06 +0100

    utilize

--- chime5/Time_sorting.py
+++ chime5/Time_sorting.py
@@ -4,14 +4,16 @@
 # @Version : 1.0
 # @Time    : 2019-07-15 17:13
 # @Author  : jacobwu
-# @File    : Time_sorting.py
+
 
 import json
 import os
-import logging
+# import logging
 import re
+import sys
+# from pprint import pprint
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
 def read_font(folder_name):
@@ -19,7 +21,7 @@ def read_font(folder_name):
     for root_folder, dirs_folder, folder in os.walk(folder_name):
         if root_folder != folder_name:
             dict_single_folder = {}
-            folder_n = root_folder.split("/")[1]
+            folder_n = re.sub('\W', ' ', root_folder).split(' ')[1]
             for f in folder:
                 file = open(root_folder + "/" + f, 'r', encoding='utf-8-sig')
                 s = json.load(file)
@@ -28,6 +30,7 @@ def read_font(folder_name):
             full_dict[folder_n] = dict_single_folder
     return full_dict
     # pass
+
 
 def file_path_f(rootDir):
     file_path = {}
@@ -40,9 +43,31 @@ def file_path_f(rootDir):
             file_path[folder_n] = dict_single_folder
     return file_path
 
-file_path_f('audio') 
 
-    # Put everyone speaking time zone into one set，Take the union，then can find the speaking time zones
+file_path_f('audio')
+
+
+def get_key(dict, value):
+    return [k for k, v in dict.items() if v == value]
+
+
+def naming(index):
+    re.compile('wav')
+    f = file_path_f('audio')
+    a = []
+    names_list = []
+    for key in f:
+        key_v = get_key(f[key], index)
+        if len(key_v):
+            a.append(key_v)
+    if len(a):
+        for fnames in a[0]:
+            fnames_l = list(fnames)
+            for i in range(4):
+                fnames_l.pop()
+            names_list.append(''.join(fnames_l))
+
+    return names_list
 
 
 def join_speak_time(speak_time):
@@ -86,35 +111,55 @@ def get_blank_time(speak_time):
 
 
 # write sox command into bash script
-def write_command_bash(filename, blank_time_zone):
-    path_f = file_path_f('audio')
-    logging.debug('writing lines to bash script {}......'.format(filename))
-    with open(filename, 'w') as fp:
+def write_command_bash(sh_name, wav_file_list, input_path, ultimate, wav_out_dir):
+
+    with open(sh_name, 'w') as fp:
         fp.write('#!/bin/bash\n')
-        commands = [
-            'sox {} output trim {} {}\n'.format(path_f_i, blank_zone[0], blank_zone[1])
-            for blank_zone in blank_time_zone
-            for path_f_i in path_f
-        ]
-        fp.writelines(commands)
-    logging.debug('writing script done!')
+        for i in range(len(wav_file_list)):
+            for j in range(len(ultimate)):
+                commands = [
+                    'sox {0} {1} trim {2} {3}\n'.format(input_path + '/' + wav_file_list[i] + '.wav',
+                                                        wav_out_dir + '/' + wav_file_list[i] + ultimate[j][0] + str(j) + '.wav',
+                                                        ultimate[j][2][0], ultimate[j][2][1])
+                ]
+                fp.writelines(commands)
+
+
+def guided_sort(index_list, target_list):
+    result_list = []
+    for j in range(len(index_list)):
+        result_list.append(target_list[index_list[j]])
+    return result_list
 
 
 if __name__ == '__main__':
     # list1 = ['dev', 'eval', 'train']
-    f_list = read_font("transcriptions")
-    # write sox command into shell script，out put shell script and save it into dict script_base_dir
-    script_base_dir = 'scripts'
 
+    # write sox command into shell script，out put shell script and save it into dict script_base_dir
+    # trans_dir = 'transcriptions'
+    # script_base_dir = 'scripts'
+    # output_path = '.'
+    # audiodir = 'audio'
+    # wav_file_output_dir = '.'
+
+    trans_dir = sys.argv[1]
+    script_base_dir = sys.argv[2]
+    output_path = sys.argv[3]
+    audiodir = sys.argv[4]
+    wav_file_output_dir = sys.argv[5]
+
+    f_list = read_font("transcriptions")
     if not os.path.exists(script_base_dir):
         os.mkdir(script_base_dir)
+
     for dirname in f_list.keys():
         # Create a subdirectory
         if not os.path.exists(script_base_dir + os.path.sep + dirname):
             os.mkdir(script_base_dir + os.path.sep + dirname)
         for filename in f_list[dirname].keys():
             # get script file name
-            script_filename = os.path.splitext(filename)[0] + '.sh'
+            S_xx = os.path.splitext(filename)[0]
+            script_filename = S_xx + '.sh'
             script_filepath = script_base_dir + os.path.sep + dirname + os.path.sep + script_filename
 
             start_time_list = []
@@ -132,15 +177,36 @@ if __name__ == '__main__':
                 end_time_list.append({f_list_end[end_time]})
 
             # output Debug information
-            logging.info('processing json file : {}'.format(dirname + os.path.sep + filename))
-            logging.debug('start time：' + str(start_time_list[0:5]))
-            logging.debug('end time:' + str(end_time_list[0:5]))
-            logging.debug('speak time:' + str(speak_time[0:5]))
+            # logging.info('processing json file : {}'.format(dirname + os.path.sep + filename))
+            # logging.debug('start time：' + str(start_time_list[0:5]))
+            # logging.debug('end time:' + str(end_time_list[0:5]))
+            # logging.debug('speak time:' + str(speak_time[0:5]))
             # get final speaking time zone
             speak_time_final = join_speak_time(speak_time)
-            logging.debug('speak zone final:' + str(speak_time_final[0:5]))
+            # logging.debug('speak zone final:' + str(speak_time_final[0:5]))
             # get no speaking time zone
             blank_time_zone = get_blank_time(speak_time_final)
-            logging.debug('blank zone:' + str(blank_time_zone[0:5]))
+            # logging.debug('blank zone:' + str(blank_time_zone[0:5]))
             # write file
-            write_command_bash(script_filepath, blank_time_zone)
+            time_len = len(blank_time_zone) + len(speak_time_final)
+            ultimate_list = []
+            for i in range(len(blank_time_zone)):
+                ultimate_list.append(['_noise_', i, blank_time_zone[i]])
+            for i in range(len(speak_time_final)):
+                ultimate_list.append(['_speech_', i + len(blank_time_zone), speak_time_final[i]])
+
+            # pprint(len(ultimate_list))
+            sorting_dict = {}
+            for i in range(len(ultimate_list)):
+                sorting_dict[ultimate_list[i][1]] = ultimate_list[i][2][0]
+            sorting_index = sorted(sorting_dict.items(), key=lambda item: item[1])
+            for i in range(len(sorting_index)):
+                sorting_index[i] = sorting_index[i][0]
+            # pprint(sorting_index)
+
+            real_ultimate_list = guided_sort(sorting_index, ultimate_list)
+            # pprint(real_ultimate_list)
+
+            write_command_bash(script_filepath, naming(S_xx), audiodir + '/' + dirname, real_ultimate_list, wav_file_output_dir)
+
+
